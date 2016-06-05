2385c933c17dd947e7c897c8c27a354844cf5bfb
Juan Oliva <sadjha@fdsd.com>
2016-06-05 13:46:42 +0000

    añadido editor ace

---
+++ public/csv.js
@@ -0,0 +1,108 @@
+// See http://en.wikipedia.org/wiki/Comma-separated_values
+(() => {
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+
+var aceditor;
+
+$(document).ready(() => {
+        aceditor = ace.edit("original");
+        aceditor.setTheme("ace/theme/sqlserver");
+        aceditor.getSession().setMode("ace/mode/javascript");
+        aceditor.setShowPrintMargin(false);
+        aceditor.session.setUseWorker(false);
+    })
+
+const resultTemplate = `
+<div class="contenido">
+      <table class="center" id="result">
+          <% _.each(rows, (row) => { %>
+          <tr class="<%=row.type%>">
+              <% _.each(row.items, (name) =>{ %>
+              <td><%= name %></td>
+              <% }); %>
+          </tr>
+          <% }); %>
+      </table>
+  </p>
+</div>
+`;
+
+/* Volcar la tabla con el resultado en el HTML */
+const fillTable = (data) => { 
+  $("#finaltable").html(_.template(resultTemplate, { rows: data.rows })); 
+};
+
+/* Volcar en la textarea de entrada 
+ * #original el contenido del fichero fileName */
+const dump = (fileName) => {
+  $.get(fileName, function (data) {
+      aceditor.setValue(data);
+  });
+};
+ 
+const handleFileSelect = (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  var files = evt.target.files;
+
+  var reader = new FileReader();
+  reader.onload = (e) => {
+
+     aceditor.setValue(e.target.result);
+  };
+  reader.readAsText(files[0])
+}
+
+/* Drag and drop: el fichero arrastrado se vuelca en la textarea de entrada */
+const handleDragFileSelect = (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  var files = evt.dataTransfer.files; // FileList object.
+
+  var reader = new FileReader();
+  reader.onload = (e) => {
+
+        aceditor.setValue(e.target.result);
+        evt.target.style.background = "white";
+  };
+  reader.readAsText(files[0])
+}
+
+const handleDragOver = (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.target.style.background = "yellow";
+}
+
+$(document).ready(() => {
+    let original = document.getElementById("original");  
+    if (window.localStorage && localStorage.original) {
+      aceditor.setValue(localStorage.original);
+    }
+
+    /* Request AJAX para que se calcule la tabla */
+    $("#parse").click(() => {
+            if (window.localStorage) localStorage.original = aceditor.getValue();
+            $.get("/csv", /* Request AJAX para que se calcule la tabla */ {
+                    input: aceditor.getValue()
+                },
+                fillTable,
+                'json'
+            );
+        });
+   /* botones para rellenar el textarea */
+   $('button.example').each( (_,y) => {
+     $(y).click( () => { dump(`${$(y).text()}.txt`); });
+   });
+
+    // Setup the drag and drop listeners.
+    //var dropZone = document.getElementsByClassName('drop_zone')[0];
+    let dropZone = $('.drop_zone')[0];
+    dropZone.addEventListener('dragover', handleDragOver, false);
+    dropZone.addEventListener('drop', handleDragFileSelect, false);
+    let inputFile = $('.inputfile')[0];
+    inputFile.addEventListener('change', handleFileSelect, false);
+ });
+})();
